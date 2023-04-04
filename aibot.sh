bd402b572c9b32cd8dac2f302b288b312074d9f5
abdza <abdullah.zainul@gmail.com>
2023-04-05 06:26:47 +0800

    Added ability to answer voice and imagine request

---
+++ aibot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /home/abdza/data/kakikoding/telegram/telegram_ai_bot/venv/bin/activate
+python /home/abdza/data/kakikoding/telegram/telegram_ai_bot/chatter.py
--- chatter.py
+++ chatter.py
@@ -3,15 +3,51 @@
 import telebot
 import openai
 import settings
+import os
+from pydub import AudioSegment
 
 bot = telebot.TeleBot(settings.telebot_key)
 openai.api_key = settings.openai_key
 
 messages = []
 
-@bot.message_handler(commands=['setup'])
+@bot.message_handler(commands=['setup','start'])
 def setup(message):
-    messages.append({'role':'user','content':'My name is ' + message.from_user.first_name})
+    messages.append({'role':'user','content':'Hello. My name is ' + message.from_user.first_name})
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages
+    )
+    messages.append(response.choices[0].message)
+    bot.reply_to(message, response.choices[0].message.content)
+
+@bot.message_handler(commands=['imagine'])
+def imagine(message):
+    response = openai.Image.create(
+        prompt=message.text,
+    n=1,
+    size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    bot.send_photo(message.chat.id, image_url)
+
+@bot.message_handler(content_types=['voice'])
+def voice_processing(message):
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    filename = 'voice_' + str(message.from_user.id)
+    with open(os.path.join('voices',filename + '.ogg'), 'wb') as new_file:
+        new_file.write(downloaded_file)
+    ogg_audio = AudioSegment.from_file(os.path.join('voices',filename + '.ogg'), format="ogg")
+    ogg_audio.export(os.path.join('voices',filename + '.mp3'), format="mp3")
+    transcript = openai.Audio.transcribe("whisper-1", open(os.path.join('voices',filename + '.mp3'),'rb'))
+    messages.append({'role':'user','content':transcript.text})
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages
+    )
+    messages.append(response.choices[0].message)
+    bot.reply_to(message, response.choices[0].message.content)
 
 @bot.message_handler()
 def catch_all(message):
