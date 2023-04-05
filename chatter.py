451122779c26dcb0922caf8db60ced8c6687b2af
abdza <abdullah.zainul@gmail.com>
2023-04-05 09:24:11 +0800

    Refactored to be asycn

--- chatter.py
+++ chatter.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-import telebot
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
 import openai
 import settings
 import os
 import textract
 from pydub import AudioSegment
 
-bot = telebot.TeleBot(settings.telebot_key)
+bot = AsyncTeleBot(settings.telebot_key)
 openai.api_key = settings.openai_key
 
 script_path = os.path.abspath(__file__)
@@ -30,32 +31,32 @@ def get_response(content):
         raise
 
 @bot.message_handler(commands=['reset'])
-def reset(message):
+async def reset(message):
     try:
         messages.clear()
         response = get_response('Hello. My name is ' + message.from_user.first_name)
-        bot.reply_to(message, response)
+        await bot.reply_to(message, response)
     except Exception as e:
-        bot.reply_to(message, "Sorry, " + str(e))
+        await bot.reply_to(message, "Sorry, " + str(e))
 
 @bot.message_handler(commands=['length','size'])
-def msg_length(message):
+async def msg_length(message):
     try:
         response = 'Message length: ' + str(len(messages)) + ' messages.'
-        bot.reply_to(message, response)
+        await bot.reply_to(message, response)
     except Exception as e:
-        bot.reply_to(message, "Sorry, " + str(e))
+        await bot.reply_to(message, "Sorry, " + str(e))
 
 @bot.message_handler(commands=['setup','start'])
-def setup(message):
+async def setup(message):
     try:
         response = get_response('Hello. My name is ' + message.from_user.first_name)
-        bot.reply_to(message, response)
+        await bot.reply_to(message, response)
     except Exception as e:
-        bot.reply_to(message, "Sorry, " + str(e))
+        await bot.reply_to(message, "Sorry, " + str(e))
 
 @bot.message_handler(commands=['imagine'])
-def imagine(message):
+async def imagine(message):
     try:
         response = openai.Image.create(
             prompt=message.text,
@@ -63,12 +64,12 @@ def imagine(message):
         size="1024x1024"
         )
         image_url = response['data'][0]['url']
-        bot.send_photo(message.chat.id, image_url)
+        await bot.send_photo(message.chat.id, image_url)
     except Exception as e:
-        bot.reply_to(message, "Sorry, " + str(e))
+        await bot.reply_to(message, "Sorry, " + str(e))
 
 @bot.message_handler(content_types=['document'])
-def document_processing(message):
+async def document_processing(message):
     try:
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
@@ -77,12 +78,12 @@ def document_processing(message):
         filetext = textract.process(os.path.join(script_dir,file_info.file_path))
         usermsg = str(message.caption) + "\nFile contents: " + str(filetext).replace('\n\n','\n')
         response = get_response(usermsg)
-        bot.reply_to(message, response)
+        await bot.reply_to(message, response)
     except Exception as e:
-        bot.reply_to(message, "Sorry, " + str(e))
+        await bot.reply_to(message, "Sorry, " + str(e))
 
 @bot.message_handler(content_types=['voice'])
-def voice_processing(message):
+async def voice_processing(message):
     try:
         file_info = bot.get_file(message.voice.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
@@ -93,16 +94,16 @@ def voice_processing(message):
         ogg_audio.export(os.path.join(script_dir,'voices',filename + '.mp3'), format="mp3")
         transcript = openai.Audio.transcribe("whisper-1", open(os.path.join(script_dir,'voices',filename + '.mp3'),'rb'))
         response = get_response(transcript)
-        bot.reply_to(message, response)
+        await bot.reply_to(message, response)
     except Exception as e:
-        bot.reply_to(message, "Sorry, " + str(e))
+        await bot.reply_to(message, "Sorry, " + str(e))
 
 @bot.message_handler()
-def catch_all(message):
+async def catch_all(message):
     try:
         response = get_response(message.text)
-        bot.reply_to(message, response)
+        await bot.reply_to(message, response)
     except Exception as e:
-        bot.reply_to(message, "Sorry, " + str(e))
+        await bot.reply_to(message, "Sorry, " + str(e))
 
-bot.infinity_polling()
+asyncio.run(bot.polling())
