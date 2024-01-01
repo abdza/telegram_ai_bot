583653350291b98c64f9be8e6906375a6697dd7a
Ahmad Zainul Abidin <ahmadclab@gmail.com>
2024-01-01 20:54:34 +0800

    Added date to recurring ticker message

--- send_message.py
+++ send_message.py
@@ -24,6 +24,7 @@ if len(subscribers)>0:
             bot.send_message(subscriber[1],toout)
         else:
             results = pd.read_csv(os.path.join(script_dir,'results_profitability.csv'))
+            results = results[results['Late Start']==0]
             results['ticker'] = "<a href='https://tradingview.com/chart?symbol=" + results['ticker'] + "'>" + results['ticker'] + "</a>"
 
             curcount = {}
@@ -70,12 +71,29 @@ if len(subscribers)>0:
                 else:
                     curcount[cticker] = 1
             sortedcount = sorted(curcount.items(), key=lambda x:x[1], reverse=True)
-            bot.send_message(subscriber[1],'Recurring Tickers\n\n' + tabulate(sortedcount,headers=['Ticker','Count']),parse_mode='HTML') #,tablefmt="grid"))
+            bot.send_message(subscriber[1],'Recurring Tickers For ' + results['date'].max() + '\n\n' + tabulate(sortedcount,headers=['Ticker','Count']),parse_mode='HTML') #,tablefmt="grid"))
 
             results.sort_values(by=['marks'],ascending=False,inplace=True)
-            tosend = results[['ticker','marks','price']]
+            tosend = results[['ticker','marks','price']].set_index('ticker')
             bot.send_message(subscriber[1],'Marks\n\n' + tabulate(tosend.iloc[:10],headers="keys"),parse_mode='HTML') #,tablefmt="grid"))
 
             results.sort_values(by=['late_marks'],ascending=False,inplace=True)
-            tosend = results[['ticker','late_marks','price']]
+            tosend = results[['ticker','late_marks','price']].set_index('ticker')
             bot.send_message(subscriber[1],'Late Marks\n\n' + tabulate(tosend.iloc[:10],headers="keys"),parse_mode='HTML') #,tablefmt="grid"))
+
+            beware = 'Beware of these props:\n'
+            beware += 'Yesterday Loss: ' + str(results['Perc Yesterday Loss'].max()) + '\n'
+            beware += 'Sluggish Ticker: ' + str(results['Perc Sluggish Ticker'].max()) + '\n'
+            beware += 'Lower High: ' + str(results['Perc Lower High'].max()) + '\n'
+            bot.send_message(subscriber[1],beware,parse_mode='HTML') #,tablefmt="grid"))
+
+            perc_prop = []
+            for col in results.columns:
+                if col[0:5] == 'Perc ':
+                    perc_prop.append(col)
+            max_prop = []
+            for col in perc_prop:
+                cperf = {'Prop':col,'Perc':results[col].max()}
+                max_prop.append(cperf)
+            max_prop.sort(key=lambda x: x['Perc'], reverse=True)
+            bot.send_message(subscriber[1],'Top Props:\n\n' + tabulate(max_prop[:20],headers='keys'),parse_mode='HTML')
